52a96d3c372f1da339fcc16b60d68d7a995d13c6
CodeVanimal <codevanimal@gmail.com>
2022-03-01 20:33:22 -0600

    React App

---
+++ frontend/src/utils/data.js
@@ -0,0 +1,191 @@
+export const categories = [
+    
+    {
+      name: 'Anime',
+      image: 'https://images.nintendolife.com/ab6fb6c4d7cc1/demon-slayer.large.jpg',
+    },
+    {
+      name: 'Art',
+      image: 'https://images.pexels.com/photos/587958/pexels-photo-587958.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    },
+    {
+      name: 'Food',
+      image: 'https://peasandcrayons.com/wp-content/uploads/2020/04/spicy-vegetarian-ramen-recipe-3.jpg'
+    },
+    {
+      name: 'Gaming',
+      image: 'https://cdn.vox-cdn.com/thumbor/aGlCvaZlBIntmux7DkDEaqU0E-M=/0x0:2040x1360/1200x675/filters:focal(857x517:1183x843)/cdn.vox-cdn.com/uploads/chorus_image/image/69494838/acastro_190618_1777_cloud_gaming_0003.0.jpg'
+    },
+    {
+      name: 'Minecraft',
+      image: 'https://www.kindpng.com/picc/m/706-7062408_liked-like-share-minecraft-logo-png-transparent-png.png',
+    },
+    
+  ];
+  
+  export const feedQuery = `*[_type == "pin"] | order(_createdAt desc) {
+    image{
+      asset->{
+        url
+      }
+    },
+        _id,
+        destination,
+        postedBy->{
+          _id,
+          userName,
+          image
+        },
+        save[]{
+          _key,
+          postedBy->{
+            _id,
+            userName,
+            image
+          },
+        },
+      } `;
+  
+  export const pinDetailQuery = (pinId) => {
+    const query = `*[_type == "pin" && _id == '${pinId}']{
+      image{
+        asset->{
+          url
+        }
+      },
+      _id,
+      title, 
+      about,
+      category,
+      destination,
+      postedBy->{
+        _id,
+        userName,
+        image
+      },
+     save[]{
+        postedBy->{
+          _id,
+          userName,
+          image
+        },
+      },
+      comments[]{
+        comment,
+        _key,
+        postedBy->{
+          _id,
+          userName,
+          image
+        },
+      }
+    }`;
+    return query;
+  };
+  
+  export const pinDetailMorePinQuery = (pin) => {
+    const query = `*[_type == "pin" && category == '${pin.category}' && _id != '${pin._id}' ]{
+      image{
+        asset->{
+          url
+        }
+      },
+      _id,
+      destination,
+      postedBy->{
+        _id,
+        userName,
+        image
+      },
+      save[]{
+        _key,
+        postedBy->{
+          _id,
+          userName,
+          image
+        },
+      },
+    }`;
+    return query;
+  };
+  
+  export const searchQuery = (searchTerm) => {
+    const query = `*[_type == "pin" && title match '${searchTerm}*' || category match '${searchTerm}*' || about match '${searchTerm}*']{
+          image{
+            asset->{
+              url
+            }
+          },
+              _id,
+              destination,
+              postedBy->{
+                _id,
+                userName,
+                image
+              },
+              save[]{
+                _key,
+                postedBy->{
+                  _id,
+                  userName,
+                  image
+                },
+              },
+            }`;
+    return query;
+  };
+  
+  export const userQuery = (userId) => {
+    const query = `*[_type == "user" && _id == '${userId}']`;
+    return query;
+  };
+  
+  export const userCreatedPinsQuery = (userId) => {
+    const query = `*[ _type == 'pin' && userId == '${userId}'] | order(_createdAt desc){
+      image{
+        asset->{
+          url
+        }
+      },
+      _id,
+      destination,
+      postedBy->{
+        _id,
+        userName,
+        image
+      },
+      save[]{
+        postedBy->{
+          _id,
+          userName,
+          image
+        },
+      },
+    }`;
+    return query;
+  };
+  
+  export const userSavedPinsQuery = (userId) => {
+    const query = `*[_type == 'pin' && '${userId}' in save[].userId ] | order(_createdAt desc) {
+      image{
+        asset->{
+          url
+        }
+      },
+      _id,
+      destination,
+      postedBy->{
+        _id,
+        userName,
+        image
+      },
+      save[]{
+        postedBy->{
+          _id,
+          userName,
+          image
+        },
+      },
+    }`;
+    return query;
+  };
